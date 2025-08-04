2da14e9f15b2944a1b011f715d030e4d7eff43da
Yazin <github-7itbcg5@yaz.in>
2025-08-04 15:55:37 +0300

    fix icons

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -18,6 +18,9 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'mic.fill': 'mic',
+  'list.bullet': 'list',
+  'gearshape.fill': 'settings',
 } as IconMapping;
 
 /**
